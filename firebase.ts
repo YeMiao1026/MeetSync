698bbb8271d34aa6d90ff24fe5d347a38442e6cd
YeMiao1026 <155522381+YeMiao1026@users.noreply.github.com>
2025-12-02 10:00:21 +0800

    refactor: Update Firebase initialization
    Removes unnecessary analytics import and uses the imported firebase object directly for initialization.

--- firebase.ts
+++ firebase.ts
@@ -1,20 +1,16 @@
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC8wpOSiloVr_bUATzbWjNCAfdh4wuJhxE",
   authDomain: "meetsync-951dc.firebaseapp.com",
   projectId: "meetsync-951dc",
   storageBucket: "meetsync-951dc.firebasestorage.app",
   messagingSenderId: "708285854650",
-  appId: "1:708285854650:web:66b0b7c7161b1ad265e383",
-  measurementId: "G-2RJ4HS4S32"
+  appId: "1:708285854650:web:66b0b7c7161b1ad265e383"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
 export const db = getFirestore(app);
